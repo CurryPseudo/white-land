c05dc507b01bb5fdc240dd52901b07e2c2cf5e80
currypseudo <currypseudo@gmail.com>
2021-10-10 21:55:51 +0800

    feat: iron land animation

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -12,7 +12,11 @@ lazy_static! {
     static ref X_DIR: Vec2 = Vec2::new(-UNIT_X, -UNIT_Y) / 2.0;
     static ref Y_DIR: Vec2 = Vec2::new(UNIT_X, -UNIT_Y) / 2.0;
 }
-pub(crate) fn spawn(mut commands: Commands, animations: Res<Assets<Animation>>) {
+pub(crate) fn spawn(
+    mut commands: Commands,
+    animations: Res<Assets<Animation>>,
+    mut sprite_sheet_animations: ResMut<Assets<benimator::SpriteSheetAnimation>>,
+) {
     commands.spawn_bundle({
         let mut b = OrthographicCameraBundle::new_2d();
         b.orthographic_projection.scale = 1.0 / 3.0; // scale to 3x
@@ -22,18 +26,22 @@ pub(crate) fn spawn(mut commands: Commands, animations: Res<Assets<Animation>>) 
     let (_, iron_land_0) = animations.iter().next().unwrap();
     {
         let anim = iron_land_0;
+        let texture_atlas = anim.atlas();
+        let anim: benimator::SpriteSheetAnimation = anim.into();
+        let anim_handle = sprite_sheet_animations.add(anim);
         for i in -3..3 {
             for j in -3..3 {
                 commands
                     .spawn_bundle(SpriteSheetBundle {
-                        texture_atlas: anim.atlas(),
+                        texture_atlas: texture_atlas.clone(),
                         transform: Transform::from_translation(Vec3::from((
                             i as f32 * *X_DIR + j as f32 * *Y_DIR,
                             0.0,
                         ))),
                         ..Default::default()
                     })
-                    .commands();
+                    .insert(anim_handle.clone())
+                    .insert(benimator::Play);
             }
         }
     }
